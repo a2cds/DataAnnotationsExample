06a351efca01ce0e9e02e188139f0ac1782b3d0c
Antonio Carlos da Silva <acdasilva@gmail.com>
2019-08-30 20:59:35 -0300

    Adicionado Display

--- DataAnnotationsExample/Models/User.cs
+++ DataAnnotationsExample/Models/User.cs
@@ -10,10 +10,16 @@ namespace DataAnnotationsExample.Models
     public class User
     {
         public int UserId { get; set; }
-        [Display()]
+        [Display(Name ="Full Name: ")]
         public string FullName { get; set; }
+        [Display(Name = "Email Address: ")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Display(Name = "Birthdate: ")]
+        [DataType(DataType.Date)]
         public DateTime Birthdate { get; set; }
+        [Display(Name = "Password: ")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
